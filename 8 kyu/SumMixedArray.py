9e8ff87b98e4661a7f06cd5ef96555cdc9e8063b
Reyyan <reyyansarikaya@gmail.com>
2021-06-22 01:22:34 +0300

    Added highest_and_lowest.py, sum_mixed_array.py and sum_of_two_lowest_positive_integers.py

--- 8 kyu/SumMixedArray.py
+++
@@ -1,9 +0,0 @@
-def sum_mix(arr):
-     """
-     Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-     Return your answer as a number.
-     """   
-   
-    result = sum([int(x) for x in arr])
-    
-    return result
---
+++ 8kyu/sum_mixed_array.py
@@ -0,0 +1,13 @@
+"""
+Author: Fatma Reyyan SARIKAYA
+"""
+"""
+ Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+ Return your answer as a number.
+"""
+
+
+def sum_mix(arr):
+    result = sum([int(x) for x in arr])
+
+    return result
